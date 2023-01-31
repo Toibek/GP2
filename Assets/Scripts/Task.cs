ee2a4036c43ed6384c2bd3b74d12a61e215715e0
kevin <kevin.hfstrm@edu.futuregames.se>
2023-01-31 15:54:37 +0100

    Tasks

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Task
+{
+    public string Name;
+    public ItemBase itemRequested;
+}
