623e4e09a4476aba268770ec09abc96e877ce481
Tobias Ek <bodentobbe@hotmail.com>
2023-02-09 14:43:28 +0100

    Gitignore and interactions(flumine)

--- Assets/Scripts/Player/Flumine.cs
+++ Assets/Scripts/Player/Flumine.cs
@@ -7,34 +7,62 @@ public class Flumine : Ability
     private Rigidbody rb;
 
     private List<Rigidbody> pickableObjects;
+    private List<Interactable> nearbyInteractable;
+    private List<PebbleCreature> nearbyLapides;
     private void Start()
     {
         rb = GetComponentInParent<Rigidbody>();
     }
     public override void Primary()
     {
-
+        for (int i = 0; i < nearbyLapides.Count; i++)
+        {
+            nearbyLapides[i].SetAwakeState(true);
+        }
     }
-
     public override void Secondary()
     {
+        for (int i = 0; i < nearbyInteractable.Count; i++)
+        {
+            nearbyInteractable[i].Interact();
+        }
+    }
+    public override void Tertiary()
+    {
+        for (int i = 0; i < nearbyLapides.Count; i++)
+        {
+            nearbyLapides[i].SetAwakeState(true);
+        }
 
     }
-
     internal void OnTriggerEnter(Collider other)
     {
-        if (other.attachedRigidbody == null) return;
-        if (pickableObjects == null) pickableObjects = new();
-        if (!pickableObjects.Contains(other.attachedRigidbody)) pickableObjects.Add(other.attachedRigidbody);
+        if (other.TryGetComponent(out AIBehavior ai))
+        {
+            if (nearbyLapides == null) nearbyLapides = new();
+            if (!nearbyLapides.Contains(ai))
+                nearbyLapides.Add(ai);
+        }
+        if (other.TryGetComponent(out Interactable interactable))
+        {
+            if (nearbyInteractable == null) nearbyInteractable = new();
+            if (!nearbyInteractable.Contains(interactable))
+                nearbyInteractable.Add(interactable);
+        }
     }
     internal void OnTriggerExit(Collider other)
     {
-        if (pickableObjects == null) pickableObjects = new();
-        if (pickableObjects.Contains(other.attachedRigidbody)) pickableObjects.Remove(other.attachedRigidbody);
-    }
-
-    public override void Tertiary()
-    {
-
+        if (other.TryGetComponent(out PebbleCreature ai))
+        {
+            if (nearbyLapides == null) nearbyLapides = new();
+            if (nearbyLapides.Contains(ai))
+                nearbyLapides.Remove(ai);
+        }
+        if (other.TryGetComponent(out Interactable interactable))
+        {
+            if (nearbyInteractable == null) nearbyInteractable = new();
+            if (nearbyInteractable.Contains(interactable))
+                nearbyInteractable.Remove(interactable);
+        }
     }
 }
