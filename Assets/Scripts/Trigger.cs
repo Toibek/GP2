21057c940e2b30dc2e19732713a74d707c83f6d3
Viktor Palmén <viktor.palmen23@gmail.com>
2023-02-09 14:27:32 +0100

    Pitcher added UI for Controls

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -4,18 +4,25 @@ using UnityEngine.UI;
 public class Trigger : MonoBehaviour
 {
     public Canvas canvas;
-    private void Start()
-    {
-        canvas = GameObject.Find("ControllsIndicator-Controller").GetComponent<Canvas>();
-    }
+
     private void OnTriggerEnter(Collider other)
     {
-        canvas.gameObject.SetActive(true);
+        canvas = other.GetComponentInChildren<Canvas>(includeInactive: true);
+
+        if (canvas != null)
+        {
+            canvas.gameObject.SetActive(true);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        canvas.gameObject.SetActive(false);
+        canvas = other.GetComponentInChildren<Canvas>(includeInactive: true);
+
+        if (canvas != null)
+        {
+            canvas.gameObject.SetActive(false);
+        }
     }
 }
 
