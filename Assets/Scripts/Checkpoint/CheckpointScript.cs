7d44f8dcc39ae59c9b98d9b7ef8bbadb5fcaf017
Mackape2 <Mackape841@gmail.com>
2023-02-09 10:49:22 +0100

    Victor <3 frank

--- Assets/Scripts/Checkpoint/CheckpointScript.cs
+++ Assets/Scripts/Checkpoint/CheckpointScript.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckpointScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         
+    //         CheckpointManager.instance.UpdateCheckPoint(gameObject);
+    //     }
+    // }
 }
