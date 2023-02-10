2bba6cbb5716ac4134821efdb862e857ab23cd61
Mackape2 <Mackape841@gmail.com>
2023-02-10 14:53:45 +0100

    After work sync 02-10

--- Assets/Scripts/Camera/CameraControll.cs
+++ Assets/Scripts/Camera/CameraControll.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 using Plane = UnityEngine.Plane;
 using Vector3 = UnityEngine.Vector3;
 
@@ -45,8 +39,11 @@ public class CameraControll : MonoBehaviour
         gm.OnPlayerJoined += GetPlayers;
         playerCount = 0;
         
+        rend = player1.GetComponent<Renderer>();
+        rend2 = player2.GetComponent<Renderer>();
         //Used by the code to remember the original position of the camera
         startPosition = transform.position;
+        
         
     }
     private void GetPlayers()
@@ -90,15 +87,13 @@ public class CameraControll : MonoBehaviour
 
     void CameraRay()
     {
-        rend = player1.GetComponent<Renderer>();
-        rend2 = player2.GetComponent<Renderer>();
         Ray ray = new Ray(currentPosition, transform.forward);
         Physics.Raycast(ray, out RaycastHit hit);
         NavMesh.SamplePosition(hit.point, out NavMeshHit navHit, 1f, NavMesh.AllAreas);
         if (navHit.hit)
             savePosition = navHit.position;
 
-            if (!VisibleFromCamera(rend, Camera.main))
+        if (!VisibleFromCamera(rend, Camera.main))
         {
             play1Pos = savePosition;
             play1Pos.y += 2;
@@ -124,13 +119,14 @@ public class CameraControll : MonoBehaviour
                 currentPosition.y = startPosition.y + play1Pos.y;
                 currentPosition.z = startPosition.z + play1Pos.z;
                 break;
+            
+            
             case 2:
-
                 jointPosition = (play1Pos + play2Pos)/2;
                     lookAtPosition = Vector3.SmoothDamp(lookAtPosition ,jointPosition,ref velocity, 0.5f);
                 currentPosition.x = jointPosition.x;
                 currentPosition.z = jointPosition.z + startPosition.z; 
-                //Makes the camera tilt up and down based on the distance between the players and the cube
+                //Makes the camera tilt up and down based on the distance between the players
                 heightDifferenceP1.y = play1Pos.y;
                 heightDifferenceP2.y = play2Pos.y;
                 currentPosition.y = (startPosition.y + (Vector3.Distance(play1Pos, play2Pos) / 5) + Vector3.Distance(heightDifferenceP1,heightDifferenceP2));
@@ -138,7 +134,6 @@ public class CameraControll : MonoBehaviour
             default:
                 break;
         }
-
         //A coded roof that keeps the camera from going to high
         if (currentPosition.y > MaxHeight)
             currentPosition.y = MaxHeight;
