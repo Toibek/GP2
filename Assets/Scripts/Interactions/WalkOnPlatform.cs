0de0ecefc0116f71776afaaccf214dca3780bdae
Mackape2 <Mackape841@gmail.com>
2023-02-15 17:10:11 +0100

    max

---
+++ Assets/Scripts/Interactions/WalkOnPlatform.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkOnPlatform : MonoBehaviour
+{
+    [Header("Will make it possible for Platforms \n to pick up this GameObject")]
+    [SerializeField] private string text = "This does nothing";
+}
